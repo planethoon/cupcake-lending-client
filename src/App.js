9396a04e531d0e1460eec23fe31df0f511518b00
Sunghoon Shin <sunghoonshin@Sunghoonui-MacBookPro.local>
2022-04-03 03:15:28 +0900

    initial setting

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <div>initial setup</div>
+    </div>
+  );
+}
+
+export default App;
