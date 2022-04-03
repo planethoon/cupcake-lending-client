e89cfdc138ce042892489f4387ab069d96c279c8
planethoon <sunghoonshin@Sunghoonui-MacBookPro.local>
2022-04-03 20:11:51 +0900

    Add borrow Tab

---
+++ src/components/BorrowTab.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+//styles below
+import FlexCenter from "./common/FlexCenter";
+import StyledBtn from "./common/StyledBtn";
+
+const BtnWrapper = styled(FlexCenter)`
+  margin-top: 30px;
+`;
+
+const Btn = styled(StyledBtn)`
+  background-color: grey;
+`;
+
+const BtnSelected = styled(Btn)`
+  background-color: pink;
+`;
+
+const BorrowTab = ({ switchToAsset, switchToLoan, curTab }) => {
+  return (
+    <BtnWrapper>
+      {curTab === "asset" ? (
+        <BtnSelected onClick={switchToAsset}>Assets</BtnSelected>
+      ) : (
+        <Btn onClick={switchToAsset}>Assets</Btn>
+      )}
+      {curTab !== "asset" ? (
+        <BtnSelected onClick={switchToLoan}>Loans</BtnSelected>
+      ) : (
+        <Btn onClick={switchToLoan}>Loans</Btn>
+      )}
+    </BtnWrapper>
+  );
+};
+
+export default BorrowTab;
