098f10cfb73567f4cf9a390675132464d6ff8e8c
planethoon <84895226+planethoon@users.noreply.github.com>
2022-04-07 00:07:20 +0900

    Add priceinfo on Assets

--- src/components/AssetList.js
+++ src/components/AssetList.js
@@ -1,29 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 // style below
 import FlexCenter from "./common/FlexCenter";
 import StyledLink from "./common/StyledLink";
 
-const AssetList = () => {
-  const dummy = [
-    { img: "https://via.placeholder.com/350" },
-    { img: "https://via.placeholder.com/250" },
-    { img: "https://via.placeholder.com/150" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/450x200" },
-    { img: "https://via.placeholder.com/250" },
-    { img: "https://via.placeholder.com/150" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-    { img: "https://via.placeholder.com/650" },
-  ];
-
+const AssetList = ({ dummy }) => {
   const Wrapper = styled.div`
     height: 400px;
     display: grid;
@@ -43,6 +25,21 @@ const AssetList = () => {
   const PreviewLink = styled(StyledLink)`
     width: 175px;
     height: 175px;
+    position: relative;
+  `;
+
+  const PreviewPrice = styled.span`
+    display: inline-block;
+    color: #f7ce00;
+  `;
+
+  const PriceWrapper = styled(FlexCenter)`
+    width: 100%;
+    height: 20px;
+    position: absolute;
+    bottom: 0px;
+
+    background-color: #666666b3;
   `;
 
   const DummyList = styled.div``;
@@ -58,6 +55,9 @@ const AssetList = () => {
           <Preview>
             <PreviewLink to="/">
               <PreviewImg src={e.img} alt={e.img} />
+              <PriceWrapper>
+                <PreviewPrice>{e.price} wETH</PreviewPrice>
+              </PriceWrapper>
             </PreviewLink>
           </Preview>
         );
--- src/pages/Borrow.js
+++ src/pages/Borrow.js
@@ -10,8 +10,37 @@ import InnerHeader from "../components/InnerHeader";
 import AssetList from "../components/AssetList";
 import LoanList from "../components/LoanList";
 
+const dummy = [
+  { img: "https://via.placeholder.com/350", price: 0.2 },
+  { img: "https://via.placeholder.com/250", price: 2 },
+  { img: "https://via.placeholder.com/150", price: 5 },
+  { img: "https://via.placeholder.com/650", price: 0.1 },
+  { img: "https://via.placeholder.com/450x200", price: 5 },
+  { img: "https://via.placeholder.com/250", price: 50 },
+  { img: "https://via.placeholder.com/150", price: 0.5 },
+  { img: "https://via.placeholder.com/650", price: 2 },
+  { img: "https://via.placeholder.com/650", price: 8 },
+  { img: "https://via.placeholder.com/650", price: 12 },
+  { img: "https://via.placeholder.com/650", price: 102 },
+  { img: "https://via.placeholder.com/650", price: 2 },
+  { img: "https://via.placeholder.com/650", price: 10 },
+  { img: "https://via.placeholder.com/650", price: 11 },
+  { img: "https://via.placeholder.com/650", price: 0.3 },
+];
+
 const Borrow = () => {
   const [curTab, setCurTab] = useState(`asset`);
+
+  // const [totalAmount, setTotalAmount] = useState(0);
+
+  // useEffect(() => {
+  //   let total;
+  //   for (let i = 0; i < dummy.length; i++) {
+  //     total = total + dummy["price"];
+  //   }
+
+  //   setTotalAmount(total);
+  // }, []);
 
   const switchToAsset = () => {
     setCurTab(`asset`);
@@ -32,7 +61,7 @@ const Borrow = () => {
           switchToLoan={switchToLoan}
           curTab={curTab}
         />
-        {curTab === `asset` ? <AssetList /> : <LoanList />}
+        {curTab === `asset` ? <AssetList dummy={dummy} /> : <LoanList />}
       </Container>
     </Background>
   );
