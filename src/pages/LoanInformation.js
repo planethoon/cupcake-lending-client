6cb05fa28c7d0cdb22e060b4584e8768b1a62a00
inijey <syd5024@gmail.com>
2022-04-12 16:17:25 +0900

    PlanList 삭제

--- src/pages/LoanInformation.js
+++ src/pages/LoanInformation.js
@@ -13,54 +13,7 @@ import LoanDetail from "../components/LoanDetail";
 import AssetList from "../components/AssetList";
 import ALoan from "../components/ALoan";
 
-const dummy = [
-  { img: "https://via.placeholder.com/350", price: 0.2 },
-  { img: "https://via.placeholder.com/250", price: 2 },
-  { img: "https://via.placeholder.com/150", price: 5 },
-  { img: "https://via.placeholder.com/650", price: 0.1 },
-  { img: "https://via.placeholder.com/450x200", price: 5 },
-  { img: "https://via.placeholder.com/250", price: 50 },
-  { img: "https://via.placeholder.com/150", price: 0.5 },
-  { img: "https://via.placeholder.com/650", price: 2 },
-  { img: "https://via.placeholder.com/650", price: 8 },
-  { img: "https://via.placeholder.com/650", price: 12 },
-  { img: "https://via.placeholder.com/650", price: 102 },
-  { img: "https://via.placeholder.com/650", price: 2 },
-  { img: "https://via.placeholder.com/650", price: 10 },
-  { img: "https://via.placeholder.com/650", price: 11 },
-  { img: "https://via.placeholder.com/650", price: 0.3 },
-];
-
-const LoanInformation = ({
-  isConnected,
-  setIsConnected,
-  account,
-  setAccount,
-  chainId,
-}) => {
-  const [curTab, setCurTab] = useState(`asset`);
-
-  // const [totalAmount, setTotalAmount] = useState(0);
-
-  // useEffect(() => {
-  //   let total;
-  //   for (let i = 0; i < dummy.length; i++) {
-  //     total = total + dummy["price"];
-  //   }
-
-  //   setTotalAmount(total);
-  // }, []);
-
-  const switchToAsset = () => {
-    setCurTab(`asset`);
-    console.log(curTab);
-  };
-
-  const switchToLoan = () => {
-    setCurTab(`loan`);
-    console.log(curTab);
-  };
-
+const LoanInformation = () => {
   return (
     <Background>
       <OuterHeader />
